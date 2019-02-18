4edc3f45d220be3fe31f6046ba3ef8720901a608
Fohlen <lennard.berger@student.uni-tuebingen.de>
2019-02-18 15:25:19 +0100

    [frontend] List samples in data table

--- frontend/src/views/Samples.vue
+++ frontend/src/views/Samples.vue
@@ -4,32 +4,75 @@
     align-top
   >
   <v-flex text-xs-center>
-    <v-list light>
-      <template v-for="(sample) in $store.getters.samples">
-        <sample v-bind="sample" v-bind:key="sample.id"></sample>
+    <v-data-table
+      :headers="headers"
+      :items="$store.getters.samples"
+    >
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.id }}</td>
+        <td>{{ props.item.author_email }}</td>
+        <td>{{ props.item.created }}</td>
+        <td>{{ props.item.last_updated }}</td>
+        <td><status :status="props.item.status"></status></td>
       </template>
-    </v-list>
+    </v-data-table>
   </v-flex>
   </v-layout>
 </template>
 
 <script>
+import Status from '@/components/Status'
 const addr = "http://localhost:4010/v1/"
-import Sample from '@/components/Sample.vue'
 
 export default {
   name: 'samples',
   components: {
-    Sample
+    Status,
+  },
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Sample ID',
+          align: 'left',
+          sortable: true,
+          value: 'id'
+        },
+        {
+          text: 'Author email',
+          align: 'left',
+          sortable: true,
+          value: 'author_email'
+        },
+        {
+          text: 'Created',
+          align: 'left',
+          sortable: true,
+          value: 'created'
+        },
+        {
+          text: 'Last updated',
+          align: 'left',
+          sortable: true,
+          value: 'last_updated'
+        },
+        {
+          text: 'Status',
+          align: 'left',
+          sortable: true,
+          value: 'status'
+        }
+      ]
+    }
   },
   mounted () {
     let vm = this
     if (window.webpackHotUpdate) {
       vm.$store.commit('addSamples', [{
         "id": 1,
-        "email": "lennard.berger@student.uni-tuebingen.de",
-        "created_at": new Date(),
-        "last_updated": new Date(),
+        "author_email": "lennard.berger@student.uni-tuebingen.de",
+        "created": new Date().toISOString(),
+        "last_updated": new Date().toISOString(),
       }])
       vm.$store.commit('addStatus', {
         "id": 1,
