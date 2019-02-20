c34f71bfe74e56b676f936dac19cdec526e6a50b
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-02-20 10:28:41 +0100

    [backend] Make port configurable

--- backend/swagger_server/__main__.py
+++ backend/swagger_server/__main__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import connexion
 
 from swagger_server import encoder
@@ -9,7 +10,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'pathoLogic'})
-    app.run(port=8080)
+    app.run(port=os.getenv('HTTP_PORT', 8080))
 
 
 if __name__ == '__main__':
