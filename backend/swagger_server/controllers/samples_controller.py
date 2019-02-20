10c6a94b3ec245a190eb8cf0e03a6cf3b2acefe4
Caspar Gross <mail@caspar.one>
2019-02-20 16:42:19 +0100

    [backend] modified sample controllers

--- backend/swagger_server/controllers/samples_controller.py
+++ backend/swagger_server/controllers/samples_controller.py
@@ -1,5 +1,10 @@
 import connexion
 import six
+import os
+import json
+#import ../.db.py as db
+
+from datetime import datetime
 
 from flask import g
 from swagger_server.models.body import Body  # noqa: E501
@@ -12,8 +17,9 @@ def format_sample(obj):
         'author_email': obj['author_email'],
         'created': obj['created'],
         'last_updated': obj['last_updated'],
-        'path_one': obj['path_one'],
-        'path_two': obj['path_two']
+        'path_lr': obj['path_lr'],
+        'path_sr1': obj['path_sr1'],
+        'path_sr2': obj['path_sr2'],
     }
 
 def samples_get():  # noqa: E501
@@ -39,5 +45,20 @@ def samples_post(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Body.from_dict(connexion.request.get_json())  # noqa: E501
-    # TODO: Create configs and add to g.db via https://pydblite.readthedocs.io/en/latest/pythonengine.html#insert-a-new-record
-    return 'do some magic!'
+    samplepath = os.path.join(os.environ.get('BASE_DIR', os.getcwd()),'samples' )
+    # Create samplepath
+    if not os.path.isdir(samplepath):
+        os.mkdir(samplepath)
+    for s in body.samples:
+        conf = body.config
+        os.mkdir(os.path.join(samplepath, s.id))
+        with open(os.path.join(samplepath, s.id, "nf_config.json"), 'w') as outfile:
+            json.dump(conf.to_dict(), outfile)
+        with open(os.path.join(samplepath, s.id, "read_locations.tsv"), 'w') as outfile:
+            outfile.write(s.id + '\t' + s.path_lr + '\t'  +
+                          s.path_sr1 + '\t' +  s.path_lr + '\n') 
+     #  db = db.get_db()
+     #  db.insert(id = s.id, author_email = s.author_email, created =
+     #               str(datetime.utcnow()), last_updated =
+     #               str(datetime.utcnow()), status = "created")
+    return body.samples
