72789085c7be9140d1ea83700f13f9809f842b62
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-02-21 10:35:40 +0100

    [backend] Pickle and unpickle db on stop / start

--- backend/swagger_server/__init__.py
+++ backend/swagger_server/__init__.py
@@ -1,5 +1,18 @@
+import atexit
+import os
+import pickle
+
+picklePath = os.path.join(os.getenv('BASE_DIR', os.getcwd()), 'pathoLogic.pickle')
 # Global dict serves as database
-db = {}
-db['samples'] = {}
-db['runs'] = {}
-db['status'] = {}
+if os.path.isfile(picklePath):
+    db = pickle.load(open(picklePath, 'rb'))
+else:
+    db = {}
+    db['samples'] = {}
+    db['runs'] = {}
+    db['status'] = {}
+
+def goodbye(db):
+    pickle.dump(db, open(os.path.join(os.getenv('BASE_DIR', os.getcwd()), 'pathoLogic.pickle'), 'wb'))
+
+atexit.register(goodbye, db)
