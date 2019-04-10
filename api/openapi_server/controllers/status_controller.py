9f56c22929b9388923fd000447a0c0282889a93d
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-10 13:17:26 +0200

    Fixed status controller. Closes #24.

--- api/openapi_server/controllers/status_controller.py
+++ api/openapi_server/controllers/status_controller.py
@@ -18,16 +18,16 @@ def status_get():  # noqa: E501
     return [{'id': sample, 'status': db['samples'][sample]['status']} for sample in db['samples'].keys()]
 
 
-def status_sample_idget(sampleID):  # noqa: E501
+def status_sample_idget(sample_id):  # noqa: E501
     """Get status for one or more samples
 
      # noqa: E501
 
-    :param sampleID: Comma seperated list of results to return
-    :type sampleID: List[str]
+    :param sample_id: Comma seperated list of results to return
+    :type sample_id: str
 
     :rtype: InlineResponse2001
     """
     db = get_db()
 
-    return [{'id': sample, 'status': db['samples'][sample]['status']} for sample in db['samples'].keys() if sample in sampleID]
+    return [{'id': sample, 'status': db['samples'][sample]['status']} for sample in db['samples'].keys() if sample in sample_id.split(',')]
