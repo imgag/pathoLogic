ffe5e83bb11023da7b150586da3309acf8440afd
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-07-29 12:51:05 +0200

    Add generated models.

--- api/openapi_server/models/__init__.py
+++ api/openapi_server/models/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 # import models into model package
 from openapi_server.models.config import Config
 from openapi_server.models.inline_object import InlineObject
+from openapi_server.models.inline_object1 import InlineObject1
 from openapi_server.models.inline_response200 import InlineResponse200
 from openapi_server.models.inline_response2001 import InlineResponse2001
 from openapi_server.models.inline_response2002 import InlineResponse2002
